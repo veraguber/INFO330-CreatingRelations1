3fff3c2a28139624badf890a8ddaa163880d6b29
VERAGUBER <VERA2@UW.EDU>
2023-04-13 10:52:43 -0700

    Completed students table

---
+++ students.sql
@@ -0,0 +1,10 @@
+CREATE TABLE students (id INTEGER PRIMARY KEY, firstname VARCHAR(40), lastname VARCHAR(80), age NUMERIC);
+
+INSERT INTO students VALUES ('1', 'Fred', 'Flintstone', '35');
+INSERT INTO students VALUES ('2', 'Wilma', 'Flintstone', '29');
+INSERT INTO students VALUES ('3', 'Barney', 'Rubble', '33');
+INSERT INTO students VALUES ('4', 'Betty', 'Rubble', '29');
+INSERT INTO students VALUES ('5', 'Pebbles', 'Flintstone', '1');
+INSERT INTO students VALUES ('6', 'Bam-Bam', 'Rubble', '1');
+INSERT INTO students VALUES ('7', 'Billy', 'Bob', '27');
+INSERT INTO students VALUES ('8', 'Joe', 'Smith', '42');
