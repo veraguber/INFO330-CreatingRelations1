26cf7664ffb10f1730190589fa76384778ab8285
VERAGUBER <VERA2@UW.EDU>
2023-04-13 13:26:34 -0700

    Completed buildings table

--- buildings.sql
+++ buildings.sql
@@ -1,2 +1,5 @@
-CREATE TABLE buildings (id SERIAL PRIMARY KEY, name VARCHAR(80) NOT NULL, shortname VARCHAR(10) UNIQUE);
+CREATE TABLE buildings (id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(80) NOT NULL, shortname VARCHAR(10) UNIQUE);
 CREATE TABLE rooms (number INTEGER, buildingid INTEGER REFERENCES buildings(id), seating INTEGER);
+
+INSERT INTO buildings (name, shortname) VALUES ('Mary Gates Hall', 'MGH');
+INSERT INTO rooms VALUES (400, (SELECT id FROM buildings WHERE name='Mary Gates Hall'), 40);
