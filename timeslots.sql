c4081f640705cde517d3ebe2fc71cf1dcb287714
VERAGUBER <VERA2@UW.EDU>
2023-04-13 20:11:57 -0700

    Completed timeslots table attempt

--- timeslots.sql
+++ timeslots.sql
@@ -1,3 +1,8 @@
 CREATE TABLE timeslots (id INTEGER PRIMARY KEY, start DATETIME NOT NULL, end DATETIME NOT NULL);
-ALTER TABLE rooms ADD COLUMN id PRIMARY KEY;
-CREATE TABLE schedule (coursecode VARCHAR(10) NOT NULL, roomid INTEGER NOT NULL REFERENCES rooms(id), timeslotid INTEGER NOT NULL REFERENCES timeslots(id), CONSTRAINT unique_room_timeslot UNIQUE (roomid, timeslotid));
+ALTER TABLE rooms ADD COLUMN id INTEGER;
+--ALTER TABLE rooms ADD CONSTRAINT room_id PRIMARY KEY (id);--
+CREATE TABLE schedule (coursecode VARCHAR(10) NOT NULL REFERENCES courses(code), roomid INTEGER NOT NULL REFERENCES rooms(id), timeslotid INTEGER NOT NULL REFERENCES timeslots(id), CONSTRAINT unique_room_timeslot UNIQUE (roomid, timeslotid));
+
+ INSERT INTO timeslots VALUES(1, '10:30:00', '11:20:00');
+ INSERT INTO timeslots VALUES(2, '11:30:00', '12:20:00');
+ INSERT INTO schedule VALUES('INFO330A', 1, 1)
